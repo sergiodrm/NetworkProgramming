553c92bd982f57719582ab91269932e592c48372
sergiodrm <47793261+sergiodrm@users.noreply.github.com>
2021-03-15 22:02:09 +0100

    Client registration in progress

--- ChatNet/Source/Client/Client.cpp
+++ ChatNet/Source/Client/Client.cpp
@@ -2,7 +2,149 @@
 //
 
 #include <iostream>
+#include "Net/factoryenet.h"
+#include "Net/buffer.h"
+
+namespace
+{
+  const int s_port = 65785;
+
+  enum class EMessageType
+  {
+    SetID,
+    SetName,
+    Message
+  };
+
+  Net::NetID s_clientID = 0;
+}
 
 int main()
 {
+  // Create the factory
+  Net::CFactory* pFactory = new Net::CFactoryEnet();
+
+  // Build a client and set it
+  Net::CClient* pClient = pFactory->buildClient();
+  pClient->init(1);
+
+  // Ask for a server IP
+  printf("Write the server IP: ");
+  char msg[1024];
+  fgets(msg, 12, stdin);
+  Net::CConnection* pConnection = pClient->connect(msg, s_port, 0, 5000u);
+
+  // Create server packets
+  std::vector<Net::CPacket*> packets;
+
+  // Once the connection succeeds, read the log in messages from server
+  bool bIsIDSet = false;
+  do
+  {
+    pClient->service(packets);
+    for (Net::CPacket* packet : packets)
+    {
+      switch (packet->getType())
+      {
+      case Net::EPacketType::DATA:
+        {
+          // Read received data
+          Net::CBuffer data;
+          data.write(packet->getData(), packet->getDataLength());
+
+          // Check data
+          EMessageType eType;
+          data.read(&eType, sizeof(eType));
+          if (eType == EMessageType::SetID)
+          {
+            // Server sent us our ID client
+            data.read(&s_clientID, sizeof(s_clientID));
+            bIsIDSet = true;
+          }
+        }
+        break;
+      }
+    }
+  }
+  while (!bIsIDSet);
+
+  // We have an ID, but we need a nick to the client user
+
+  printf("Write a nick: ");
+  fgets(msg, 1023, stdin);
+  char* endLine = strchr(msg, '\n');
+  *endLine = '\0';
+  Net::CBuffer data;
+  EMessageType eMessageType = EMessageType::SetName;
+  data.write(&eMessageType, sizeof(eMessageType));
+  data.write(&s_clientID, sizeof(s_clientID));
+  data.write(msg, strlen(msg));
+  Net::CPacket packet(Net::EPacketType::DATA, data.getData(), data.getSize(), pConnection, 0);
+  pClient->sendData(pConnection, packet.getData(), packet.getDataLength(), 0, true);
+
+  printf("Client id sent to server: #%d: %s\n", s_clientID, msg);
+
+
+  // Client loop
+  printf("Write text to send (\"exit\" to close connection):\n");
+  do
+  {
+    // Read server packets
+    pClient->service(packets);
+    for (Net::CPacket* packet : packets)
+    {
+      switch (packet->getType())
+      {
+      case Net::CONNECTION:
+        {
+          printf("You has been connected successfully!\n");
+        }
+        break;
+      case Net::DATA:
+        {
+          Net::CBuffer data;
+          data.write(packet->getData(), packet->getDataLength());
+          data.reset();
+          size_t length = data.getSize();
+          data.read(msg, length);
+          msg[length] = '\0';
+          printf("Message from server: %s\n", msg);
+        }
+        break;
+      case Net::DISCONNECTION:
+        {
+          printf("You has been disconnected\n");
+        }
+        break;
+      default: break;
+      }
+    }
+
+    // Send client data
+
+    // Give me the message
+    fgets(msg, 1023, stdin);
+
+    // Find end line and change it
+    char* endLine = strchr(msg, '\n');
+    *endLine = '\0';
+
+    if (strcmp(msg, "exit") != 0)
+    {
+      Net::CBuffer data;
+      data.write(msg, strlen(msg));
+      Net::CPacket messagePacket(Net::EPacketType::DATA, data.getData(), data.getSize(), pConnection, 0);
+      pClient->sendData(pConnection, messagePacket.getData(), messagePacket.getDataLength(), 0, true);
+    }
+  }
+  while (strcmp(msg, "exit") != 0);
+
+  printf("Closing client app\n");
+
+  // Clear pointers
+  pClient->release();
+  delete pClient;
+  delete pFactory;
+
+  return 0;
 }
--- ChatNet/Source/Server/Server.cpp
+++ ChatNet/Source/Server/Server.cpp
@@ -3,7 +3,113 @@
 
 #include <iostream>
 
-int main()
+#include "Net/buffer.h"
+#include "Net/factoryenet.h"
+
+namespace
 {
+  const int s_port = 65785;
+
+  enum class EMessageType
+  {
+    SetID,
+    SetName,
+    Message
+  };
+
+  Net::NetID s_NextID = 0;
 }
 
+int main()
+{
+  // Create a factory to make a server
+  Net::CFactory* pFactory = new Net::CFactoryEnet();
+
+  // Build server
+  Net::CServer* pServer = pFactory->buildServer();
+  pServer->init(s_port, 10);
+
+  printf("Server listening\n");
+  // Server loop
+  std::vector<Net::CPacket*> packets;
+  char msg[1024];
+  do
+  {
+    // Give me the packets
+    pServer->service(packets);
+
+    // Process packets
+    for (std::vector<Net::CPacket*>::iterator iterator = packets.begin(); iterator != packets.end(); ++iterator)
+    {
+      Net::CPacket* pPacket = *iterator;
+      switch (pPacket->getType())
+      {
+      case Net::CONNECTION:
+        {
+          ++s_NextID;
+          Net::CBuffer data;
+          EMessageType eType = EMessageType::SetID;
+          data.write(&eType, sizeof(eType));
+          data.write(&s_NextID, sizeof(s_NextID));
+          Net::CPacket packet(Net::EPacketType::DATA, data.getData(), data.getSize(), pPacket->getConnection(), 0);
+          pServer->sendData(pPacket->getConnection(), packet.getData(), packet.getDataLength(), 0, true);
+          printf("New client connection!\n");
+        }
+        break;
+      case Net::DATA:
+        {
+          // We have to check the type of data which we have received
+          Net::CBuffer data;
+          data.write(pPacket->getData(), pPacket->getDataLength());
+          data.reset();
+          // Read message type
+          EMessageType eMessageType;
+          data.read(&eMessageType, sizeof(eMessageType));
+          // Read client ID
+          Net::NetID clientID;
+          data.read(&clientID, sizeof(clientID));
+          // Calculate the size of the rest of the packet
+          size_t size = data.getSize() - sizeof(eMessageType) - sizeof(clientID);
+          data.read(msg, size);
+          msg[size] = '\0';
+
+          // Interpret the msg
+          switch (eMessageType)
+          {
+          case EMessageType::Message:
+            {
+              printf("[Client %d] Received text: %s\n", clientID, msg);
+            }
+            break;
+          case EMessageType::SetName:
+            { }
+            break;
+          default: break;
+          }
+
+          // Resend to all
+          pServer->sendAll(pPacket->getData(), pPacket->getDataLength(), 0, true);
+        }
+        break;
+      case Net::DISCONNECTION:
+        {
+          printf("Client disconnected\n");
+        }
+        break;
+      default: break;
+      }
+    }
+
+    // Clear current packets
+    packets.clear();
+  }
+  while (strcmp(msg, "close_server") != 0);
+
+  printf("Closing server\n");
+
+  pServer->release();
+  delete pServer;
+  delete pFactory;
+
+  return 0;
+}
